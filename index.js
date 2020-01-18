9fa61399f9b8586b641f6ffa5617678f18f3e117
mteachey <melindateachey@gmail.com>
2020-01-18 10:33:26 -0700

    created add item, remove item, check  item to js

--- index.js
+++ index.js
@@ -1,31 +1,34 @@
-$(function() {
-    $('#number-chooser').submit(event => {
-      // this stops the default form submission behavior
-      event.preventDefault();
-      // in case there's already results; this was in solution not mine
-      $(".js-results").empty();
-      console.log('is this working');
-      const userTextElement = $(this).find('#number-choice');
-      const userNumberInput = userTextElement.val();
 
-      //solution const num = parseInt( $(event.currentTarget).find('input[name="number-choice"]').val());
-      console.log(`userNumber is ${userNumberInput}`);
-      let specifiedValue = 1;
-      for (i=1;i<=userNumberInput;i++ ){   
-        if (i%15===0){
-            specifiedValue = 'fizzbuzz';
-        }
-        else if(i%3===0){
-            specifiedValue = 'fizz';
-        }
-        else if(i%5===0){
-            specifiedValue = 'buzz';   
-         }
-        else{
-            specifiedValue = i;    
-         }
-         $('.js-results').append(`<div class="fizz-buzz-item"><span>${specifiedValue}</span></div>`); 
-    };//end of for loop
-    
-    });  
-  })
+$(function() {
+//add items
+    $( "#js-shopping-list-form" ).submit(function( event ) {
+        event.preventDefault();
+        const newItem = $(this).find('input[name="shopping-list-entry"]').val();
+      console.log(`new item entered is ${newItem}`);
+      $('.shopping-list').append(`<li>
+      <span class="shopping-item">${newItem}</span>
+        <div class="shopping-item-controls">
+          <button class="shopping-item-toggle">
+            <span class="button-label">check</span>
+          </button>
+          <button class="shopping-item-delete">
+            <span class="button-label">delete</span>
+          </button>
+        </div>
+       </li>`);         
+      });
+
+//toggle checked item including new ones
+     //$(".shopping-item-toggle").click(function(){
+    $('.shopping-list').on('click', '.shopping-item-toggle', function(event) {
+        $(this).parent().siblings('span').toggleClass("shopping-item__checked");
+        console.log('checked clicked');
+      });
+//remove item including new ones
+     // $(".shopping-item-delete").click(function(){
+        $('.shopping-list').on('click', '.shopping-item-delete', function(event) {
+          $(this).closest('li').remove('li');
+          console.log('delete clicked');
+      });
+ 
+  })//end of onload function
